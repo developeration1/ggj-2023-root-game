0696f6b88985cf9499c568427ea6d286c6996b47
jorgemerlo <jorgemerlosantos@gmail.com>
2023-02-05 00:46:34 -0600

    Movimiento con space, colisiones y que no se pueda mover hacia adentro de la rama

--- Assets/Scripts/rootController.cs
+++ Assets/Scripts/rootController.cs
@@ -9,10 +9,12 @@ public class rootController : MonoBehaviour
     public LineRenderer lineRenderer;
     public float movementQuantity = 10;
     private int lineRendererPositions = 1;
+    private Vector3 movementOutput;
     // Start is called before the first frame update
     void Start()
     {
-        
+        movementOutput = new Vector3(0, -1, 0) * movementQuantity;
+        MoveRoot();
     }
 
     // Update is called once per frame
@@ -20,17 +22,58 @@ public class rootController : MonoBehaviour
     {
         
     }
-    public void MoveRoot(InputAction.CallbackContext context)
+
+
+
+    public void RootDirection(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
-            lineRendererPositions++;
-            lineRenderer.positionCount = lineRendererPositions;
             Vector2 movementInput = context.ReadValue<Vector2>();
-            Vector3 movementOutput = new Vector3(movementInput.x, movementInput.y, 0) * movementQuantity;
-            controller.Move(movementOutput);
-            lineRenderer.SetPosition(lineRendererPositions-1,controller.transform.position);
-            
+            if (movementOutput.x == null || movementOutput.x == 0 || (movementOutput.x * -1) != movementInput.x)
+            {
+                movementOutput = new Vector3(movementInput.x, movementInput.y, 0) * movementQuantity;
+            }
         }
+    }
+
+    public void BeatPress(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            //if(not on time){
+            //controller.Move hacia abajo
+            //quita vida
+            //}else
+            //{
+
+            //}
+            MoveRoot();
+        }
+    }
+
+    public void MoveRoot()
+    {
+        lineRendererPositions++;
+        lineRenderer.positionCount = lineRendererPositions;
+        controller.Move(movementOutput);
+        lineRenderer.SetPosition(lineRendererPositions - 1, controller.transform.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("Hubo colision con " + other.tag);
+        string tag = other.tag;
+        switch (tag.ToLower())
+        {
+            case "agua":
+                Debug.Log("Es agua");
+            break;
+            case "piedra":
+                Debug.Log("Es piedra");
+            break;
+
+        }
+            
     }
 }
